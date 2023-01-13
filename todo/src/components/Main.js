45149cef9c178d596b20bf7898e68187df9e2003
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-13 13:17:24 +0100

    styling

--- todo/src/components/Main.js
+++ todo/src/components/Main.js
@@ -4,9 +4,11 @@ export default function Main() {
   return (
     <div>
         <div className='row justify-content-center'>
-            <h1>React To Do List</h1>
-            <input type='text' placeholder='Enter Task' className='form-control'/>
-            <button className='btn btn-success'>ADD</button>
+            <div className='col-md-5 shadow-lg p-3 mb-5 bg-white rounded'>
+                <h1>React To Do List</h1>
+                <input type='text' placeholder='Enter Task' className='form-control'/>
+                <button className='btn btn-success'>ADD</button>
+            </div>
         </div>
     </div>
   )
